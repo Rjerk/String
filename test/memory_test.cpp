edc274f728b6360027a0fe718d2d710510b72881
Rjerk <460139918@qq.com>
2017-06-21 23:16:21 +0800

    test

---
+++ test/memory_test.cpp
@@ -0,0 +1,32 @@
+#include "../Memory.h"
+#include <iostream>
+
+template <typename T>
+struct Ptr {
+    T* pad;
+    T* data;
+    Ptr(T* arg): pad(nullptr), data(arg)
+    {
+        std::cout << "Ctor this = " << this << std::endl;
+    }
+    ~Ptr() { delete data; }
+    T** operator&() { return &data; }
+};
+
+template <typename T>
+void f(Ptr<T>* p)
+{
+    std::cout << "Ptr overload called with p = " << p << "\n";
+}
+
+void f(int** p)
+{
+    std::cout << "int** overload called with p = " << p << "\n";
+}
+
+int main()
+{
+    Ptr<int> p(new int(35));
+    f(&p);
+    f(mystl::addressof(p));
+}
