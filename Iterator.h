d9b53343bbafd85bd0b9a992d3aece1779a08dfd
Rjerk <460139918@qq.com>
2017-06-21 23:15:48 +0800

    iterator

--- Iterator.h
+++ Iterator.h
@@ -17,7 +17,7 @@ struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 
 template<typename Category, typename T, typename Distance = ptrdiff_t,
            typename Pointer = T*, typename Reference = T&>
-struct iterator {
+struct Iterator {
       using iterator_category = Category;
       using value_type = T;
       using difference_type = Distance;
@@ -51,6 +51,31 @@ struct iterator_traits<const T*> {
 	using pointer = const T*;
 	using reference = const T&;
 };
+
+template <typename InputIterator, class Distance>
+    void advance(InputIterator& i, Distance n);
+template <typename InputIterator>
+    typename iterator_traits<InputIterator>::difference_type
+    distance(InputIterator first, InputIterator last);
+template <typename ForwardIterator>
+    ForwardIterator next(ForwardIterator x,
+        typename iterator_traits<ForwardIterator>::difference_type n = 1);
+template <typename BidirectionalIterator>
+    BidirectionalIterator prev(BidirectionalIterator x,
+        typename iterator_traits<BidirectionalIterator>::difference_type n = 1);
+
+template <typename Iterator>
+class reverse_iterator;
+
+template <typename Iterator1, typename Iterator2>
+bool operator==(const reverse_iterator<Iterator1>& x,
+                const reverse_iterator<Iterator2>& y);
+
+
+
+
+template <typename C>
+auto begin(C& c) -> decltype(c.begin());
 
 }
 
